5e7620d162abdf73a4ef554e5a87dcf001e8193e
yasserelsayed70 <yasserelsayed70@gmail.com>
2023-09-05 14:48:16 +0300

    First Commit

---
+++ Assets/Scripts/UI/TestingNetcodeUI.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Netcode;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TestingNetcodeUI : MonoBehaviour
+{
+    [SerializeField] Button startHostButton;
+    [SerializeField] Button startClientButton;
+
+    private void Awake()
+    {
+        startHostButton.onClick.AddListener(() =>
+        {
+            Debug.Log("Host");
+            NetworkManager.Singleton.StartHost();
+            Hide();
+        });
+        startClientButton.onClick.AddListener(() =>
+        {
+            Debug.Log("Client");
+            NetworkManager.Singleton.StartClient();
+            Hide();
+        });
+    }
+    private void Hide()
+    {
+        gameObject.SetActive(false);
+    }
+}
